ae6d77213530816201c5fa1a735e997596a8f1d4
zhaoyiming <yiming_zhao@qingsongchou.com>
2018-09-01 14:24:18 +0800

    Init

---
+++ src/rem.js
@@ -0,0 +1,23 @@
+
+/**
+ * Description: 移动端rem适配方案
+ * User: zhaoyiming
+ * Date: 2018/09/01
+ * License: MIT, https://github.com/zymseo/rem
+ */
+;(function (window) {
+	var dpr = window.devicePixelRatio || 1,
+		width = 750,
+		docEle = window.document.documentElement;
+	
+	var resizeCall = (function () {
+		var clientWidth = docEle.clientWidth;
+		docEle.style.fontSize = 100 * (clientWidth / parseInt(width)) + 'px';
+		return arguments.callee;
+	})();
+
+	dpr = dpr >= 3 ? 3 : dpr >= 2 ? 2 : 1;
+	docEle.setAttribute('data-dpr', dpr);
+
+	window.addEventListener('resize', resizeCall, false);
+ })(this);
